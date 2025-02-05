c36339dc872e87544e16c162c5ea6e1d20692f3b
Ricardo Gallegos <ricardo.gallegos@usach.cl>
2025-02-05 20:54:13 -0300

    funciones implementadas

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+var listaAmigos = [];
+
+function mostrarSorteado(amigoSorteado) {
+    document.getElementById("listaAmigos").innerHTML =
+                                `<h1> El Sorteado es: </h1>
+                                <ul> ${amigoSorteado} </ul>`;
+}
+
+function sortearAmigo() {
+    let seleccionado = Math.random() * listaAmigos.length;
+    mostrarSorteado(listaAmigos[seleccionado]);
+
+}
+
+function actualizarListaFront() {
+    let listaFront = document.getElementById("listaAmigos");
+    console.log(listaFront);
+    listaFront.innerHTML = "";
+    listaAmigos.forEach( amigo => {
+        listaFront.innerHTML += `<ul> ${amigo} </ul>`;
+    });
+}
+
+function agregarAmigo() {
+    let nuevoAmigo = document.getElementById("amigo").value;
+    console.log(nuevoAmigo);
+    if ( nuevoAmigo != "" && nuevoAmigo ){
+        listaAmigos.push(nuevoAmigo);
+//        console.log(`nuevo amigo: ${nuevoAmigo}`);
+    }
+    actualizarListaFront();
+}
