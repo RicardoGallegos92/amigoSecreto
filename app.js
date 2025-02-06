817f8c369d6df5fba491db084beb358a4042dbcb
Ricardo Gallegos <ricardo.gallegos@usach.cl>
2025-02-05 21:09:12 -0300

    correciones para front

--- app.js
+++ app.js
@@ -9,26 +9,36 @@ function mostrarSorteado(amigoSorteado) {
 }
 
 function sortearAmigo() {
-    let seleccionado = Math.random() * listaAmigos.length;
-    mostrarSorteado(listaAmigos[seleccionado]);
-
+    if ( listaAmigos.length == 0 ){
+        mostrarSorteado("No hay amigos en la lista aún");
+        return;
+    }else{
+        let seleccionado = Math.round( Math.random() * (listaAmigos.length - 1) ) ;
+//    console.log(`Seleccionado : ${seleccionado}`);
+        mostrarSorteado(listaAmigos[seleccionado]);
+    }
 }
 
 function actualizarListaFront() {
     let listaFront = document.getElementById("listaAmigos");
-    console.log(listaFront);
+//    console.log(listaFront);
     listaFront.innerHTML = "";
     listaAmigos.forEach( amigo => {
         listaFront.innerHTML += `<ul> ${amigo} </ul>`;
     });
 }
 
+function limpiarInput() {
+    document.getElementById("amigo").value = "";
+}
+
 function agregarAmigo() {
     let nuevoAmigo = document.getElementById("amigo").value;
-    console.log(nuevoAmigo);
+//    console.log(nuevoAmigo);
     if ( nuevoAmigo != "" && nuevoAmigo ){
         listaAmigos.push(nuevoAmigo);
 //        console.log(`nuevo amigo: ${nuevoAmigo}`);
     }
     actualizarListaFront();
+    limpiarInput();
 }
